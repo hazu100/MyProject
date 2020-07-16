65d8a739b2c8b664b36f344a541113c4ae713736
Abhisek <abhisek.hazra.95@gmail.com>
2020-07-17 01:22:54 +0530

    Added static folder for production

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ databaseConnect();
 //reuest body parser
 app.use(express.json({ expanded: false }));
 
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+}
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
 app.use('/api/registration', require('./routes/api/user'));
 app.use('/api/auth', require('./routes/api/auth'));
 
